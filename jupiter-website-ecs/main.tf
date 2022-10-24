894918bd8a2c8de1dc873a3276a8cddbf29029d8
Noah Parker <noah98parker@gmail.com>
2022-10-23 22:57:21 -0400

    create alb

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -46,3 +46,13 @@ module "acm" {
   domain_name       = var.domain_name
   alternative_name  = var.alternative_name
 }
+
+module "application_load_balancer" {
+  source                = "../Modules/alb"
+  project_name          = module.vpc.project_name
+  alb_security_group_id = module.security_group.alb_security_group_id
+  public_subnet_az1_id  = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id  = module.vpc.public_subnet_az2_id
+  vpc_id                = module.vpc.vpc_id
+  certificate_arn       = module.acm.certificate_arn
+}
